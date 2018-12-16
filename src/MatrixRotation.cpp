34694855cc9b4c9e9e44face21a5300c9752355b
Tom Goring <tgoring97@gmail.com>
2018-12-16 15:56:45 +0000

    Add unfinished MatrixRotation.cpp

---
+++ src/MatrixRotation.cpp
@@ -0,0 +1,27 @@
+//
+// Created by Tom Goring on 16/12/2018.
+//
+
+#define N 4
+
+#include <iostream>
+
+void rotateMatrix(int matrix[N][N]);
+
+
+int main() {
+
+    int matrix[N][N] = {
+            { 1,2,3,4 },
+            { 5,6,7,8 },
+            { 9,10,11,12 },
+            { 13,14,15,16 }
+    };
+
+    rotateMatrix(matrix);
+}
+
+void rotateMatrix(int matrix[N][N]) {
+
+
+}
